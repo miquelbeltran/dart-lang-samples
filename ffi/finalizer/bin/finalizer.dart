9154679970e72b596b0e7f7299a914204f5b7bbb
Miguel Beltran <m@beltran.work>
2022-03-29 16:03:46 +0200

    Add empty project

---
+++ ffi/finalizer/bin/finalizer.dart
@@ -0,0 +1,5 @@
+import 'package:finalizer/finalizer.dart' as finalizer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${finalizer.calculate()}!');
+}
---
+++ ffi/finalizer/test/finalizer_test.dart
@@ -0,0 +1,8 @@
+import 'package:finalizer/finalizer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
